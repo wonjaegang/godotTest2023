6f22a02ad0603c6ed19b1c5cb7347748a88c7e14
wonjaegang <52266110+wonjaegang@users.noreply.github.com>
2023-12-17 17:44:05 +0900

    Add MenuScene/Button

--- scripts/steve.gd
+++ scripts/steve.gd
@@ -4,38 +4,38 @@ var firstTouchXY = Vector2(0, 0)
 const SPEED = 5
 
 func _physics_process(delta):
-    if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-        if not isTouching:
-            firstTouchXY = get_viewport().get_mouse_position()
-            isTouching = true
-        else:
-            var currentTouchXY = get_viewport().get_mouse_position()
-            if currentTouchXY != firstTouchXY:
-                var angle = firstTouchXY.angle_to_point(currentTouchXY)
-                velocity.x = cos(angle) * SPEED
-                velocity.z = sin(angle) * SPEED
-            
-    else:
-        velocity = lerp(velocity, Vector3(0, 0, 0), 0.2)
-        isTouching = false
-    move_and_slide()
-    $MeshInstance3D.rotate_z(-velocity.x / scale.x * 2 * delta)
-    $MeshInstance3D.rotate_x(velocity.z / scale.x * 2 * delta)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		if not isTouching:
+			firstTouchXY = get_viewport().get_mouse_position()
+			isTouching = true
+		else:
+			var currentTouchXY = get_viewport().get_mouse_position()
+			if currentTouchXY != firstTouchXY:
+				var angle = firstTouchXY.angle_to_point(currentTouchXY)
+				velocity.x = cos(angle) * SPEED
+				velocity.z = sin(angle) * SPEED
+			
+	else:
+		velocity = lerp(velocity, Vector3(0, 0, 0), 0.2)
+		isTouching = false
+	move_and_slide()
+	$MeshInstance3D.rotate_z(-velocity.x / scale.x * 2 * delta)
+	$MeshInstance3D.rotate_x(velocity.z / scale.x * 2 * delta)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
