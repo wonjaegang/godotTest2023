08b5a974ff7f2a28ade12e5a7a6a4e47192898d6
wonjaegang <robinsam@naver.com>
2023-12-18 17:46:45 +0900

    add enemy, gameover scene

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -2,6 +2,6 @@ extends Camera3D
 var destination = Vector3(0, 0, 0)
 
 func _physics_process(delta):
-	destination = $'../Steve'.position + Vector3(0, 5, 5)
-	position = lerp(position, destination, 0.1)
-	
+    destination = $'../Steve'.position + Vector3(0, 5, 5)
+    position = lerp(position, destination, 0.1)
+    
--- scripts/steve.gd
+++ scripts/steve.gd
@@ -3,39 +3,35 @@ var isTouching = false
 var firstTouchXY = Vector2(0, 0)
 const SPEED = 5
 
+
 func _physics_process(delta):
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		if not isTouching:
-			firstTouchXY = get_viewport().get_mouse_position()
-			isTouching = true
-		else:
-			var currentTouchXY = get_viewport().get_mouse_position()
-			if currentTouchXY != firstTouchXY:
-				var angle = firstTouchXY.angle_to_point(currentTouchXY)
-				velocity.x = cos(angle) * SPEED
-				velocity.z = sin(angle) * SPEED
-			
-	else:
-		velocity = lerp(velocity, Vector3(0, 0, 0), 0.2)
-		isTouching = false
-	move_and_slide()
-	$MeshInstance3D.rotate_z(-velocity.x / scale.x * 2 * delta)
-	$MeshInstance3D.rotate_x(velocity.z / scale.x * 2 * delta)
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+    if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+        if not isTouching:
+            firstTouchXY = get_viewport().get_mouse_position()
+            isTouching = true
+        else:
+            var currentTouchXY = get_viewport().get_mouse_position()
+            if currentTouchXY != firstTouchXY:
+                var angle = firstTouchXY.angle_to_point(currentTouchXY)
+                velocity.x = cos(angle) * SPEED
+                velocity.z = sin(angle) * SPEED
+            
+    else:
+        velocity = lerp(velocity, Vector3(0, 0, 0), 0.2)
+        isTouching = false
+    move_and_slide()
+    $Sphere.rotate_z(-velocity.x / scale.x * 2 * delta)
+    $Sphere.rotate_x(velocity.z / scale.x * 2 * delta)
+
+
+func _on_enemy_body_entered(body):
+    if body.name == 'Steve':
+        get_tree().change_scene_to_file('res://gameover.tscn')
+
+
+
+
+
+
+
+
